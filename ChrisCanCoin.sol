b41849d6ed5036eabffee49fb463da47f3b7b4ec
Chris Wysocki <chris@Chriss-MacBook-Pro.local>
2022-08-01 19:13:00 -0500

    Created a simple ERC20 coin on the Ropsten testnet. Contract  0x509cAbB92b5f9c94B2e1EC10af8fc7dB96605670

---
+++ ChrisCanCoin.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+// import the ERC20 Contract from OpenZeppelin
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+// Create the contract ChrisCanCoin as a ERC20 to inherit the contract standards
+contract ChrisCanCoin is ERC20 {
+
+    // Construct the token and give it the name Chris Can Coin and symbol CCC
+    // Note that the constructor is set to only deploy the CCC token.
+    // If I wanted to write a more dynamic constructor I would use the following code:
+    // constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol)
+    constructor() ERC20("Chris Can Coin", "CCC"){
+
+        // Mint a supply of 100 CCC tokens to my address
+        _mint(msg.sender, 100 * (10 ** 18));
+
+    }
+
+}
